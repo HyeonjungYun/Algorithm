3fca3bffd470cd0123ea65955c3d15b5b1f50062
윤현중 <58295105+HyeonjungYun@users.noreply.github.com>
2025-04-28 12:13:44 +0900

    [level 3] Title: 인사고과, Time: 139.81 ms, Memory: 27 MB -BaekjoonHub

---
+++ 프로그래머스/3/152995. 인사고과/인사고과.cpp
@@ -0,0 +1,47 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <iostream>
+
+using namespace std;
+
+int solution(vector<vector<int>> scores) {
+    int answer = 1;
+    
+    vector<vector<int>> survivors;
+    pair<int, int> key = make_pair(scores[0][0], scores[0][1]);
+    
+    sort(scores.begin(), scores.end(), [](vector<int> a, vector<int> b)
+         {
+             if (a[0] != b[0]) return a[0] > b[0];
+             return a[1] < b[1];
+         });
+    
+    int max_b = -1;
+    for (vector<int>& score : scores)
+    {
+        if (score[1] >= max_b)
+        {
+            max_b = score[1];
+            survivors.push_back(score);
+        }
+        else
+        {
+            if (score[0] == key.first && score[1] == key.second)
+            {
+                return -1;
+            }
+        }
+    }
+    
+    for (vector<int>& score : survivors)
+    {
+        if ((key.first + key.second) < (score[0] + score[1]))
+        {
+            answer++;
+        }
+    }
+    
+    return answer;
+}
