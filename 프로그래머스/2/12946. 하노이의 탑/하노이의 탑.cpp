3eb5e72ef5a61e216bff19118414b6960179f37c
윤현중 <58295105+HyeonjungYun@users.noreply.github.com>
2025-04-29 14:22:15 +0900

    [level 2] Title: 하노이의 탑, Time: 44.43 ms, Memory: 28 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.cpp
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.cpp
@@ -1,14 +1,16 @@
 #include <string>
 #include <vector>
-#include <stack>
 
 using namespace std;
 
 vector<vector<int>> discMove(int x, int y, int z, int n) {
     vector<vector<int>> moves;
-    if(n == 1) {
+    if(n == 1) 
+    {
         moves.push_back({x, z});
-    } else {
+    } 
+    else 
+    {
         // 첫 번째 재귀 호출: x에서 y로 옮기기 (보조 기둥: z)
         vector<vector<int>> left = discMove(x, z, y, n - 1);
         // 두 번째 재귀 호출: y에서 z로 옮기기 (보조 기둥: x)
