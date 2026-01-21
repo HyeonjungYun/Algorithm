719ea915de37851a1d529c177a4e8ea8cdeeef62
윤현중 <58295105+HyeonjungYun@users.noreply.github.com>
2026-01-21 11:44:24 +0900

    [level 1] Title: 모의고사, Time: 0.11 ms, Memory: 4.45 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.cpp
+++ 프로그래머스/1/42840. 모의고사/모의고사.cpp
@@ -1,40 +1,40 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-void submission(vector<int> &out, int size, int num) {
-    vector<int> num1 = {1,2,3,4,5};
-    vector<int> num2 = {2,1,2,3,2,4,2,5};
-    vector<int> num3 = {3,3,1,1,2,2,4,4,5,5};
-    
-    for (int i = 0; i < size; i++) {
-        if (num == 1) out.push_back(num1[i % 5]);
-        if (num == 2) out.push_back(num2[i % 8]);
-        if (num == 3) out.push_back(num3[i % 10]);
-    }
-}
-
 vector<int> solution(vector<int> answers) {
     vector<int> answer;
-    vector<int> correctAnswer;
-    int max = 0;
+    vector<int> n1 = {1,2,3,4,5};
+    vector<int> n2 = {2,1,2,3,2,4,2,5};
+    vector<int> n3 = {3,3,1,1,2,2,4,4,5,5};
+    vector<int> ansNum(3, 0);
     
-    for (int i = 1; i <= 3; i++) {
-        vector<int> submitter;
-        submission(submitter, answers.size(), i);
-        
-        int correct = 0;
-        for (int j = 0; j < answers.size(); j++) 
-            if (submitter[j] == answers[j]) correct++;
-        
-        if (max < correct) max = correct;
-        
-        correctAnswer.push_back(correct);
+    for (int i = 0; i < answers.size(); ++i)
+    {
+        if (answers[i] == n1[i % n1.size()])
+        {
+            ++ansNum[0];
+        }
+        if (answers[i] == n2[i % n2.size()])
+        {
+            ++ansNum[1];
+        }
+        if (answers[i] == n3[i % n3.size()])
+        {
+            ++ansNum[2];
+        }
     }
     
-    for (int i = 0; i < 3; i++)
-        if (max == correctAnswer[i]) answer.push_back(i + 1);
+    int max = *max_element(ansNum.begin(), ansNum.end());
+    for (int i = 0; i < 3; ++i)
+    {
+        if (max == ansNum[i])
+        {
+            answer.push_back(i + 1);
+        }
+    }
     
     return answer;
 }
